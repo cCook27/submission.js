eeb1d17413895e39e3ae2ae064fe35ac97ee69cb
cCook27 <110147218+cCook27@users.noreply.github.com>
2022-09-16 22:24:01 -0700

    Update submission.js

--- submission.js
+++ submission.js
@@ -46,5 +46,46 @@ const removeParenth = function(str) {
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+
+  pointsAwarded = 0
+
+  const organizeInputs = function(onePoint, twoPoint, threePoint, fourPoint, fivePoint, eightPoint, tenPoint){
+    pointsArray = [];
+    mainPointArr = [onePoint, twoPoint, threePoint, fourPoint, fivePoint, eightPoint, tenPoint]
+    for (let i = 0; i < mainPointArr.length; i++) {
+      const element = pointsArray.push(String(mainPointArr[i]).replace(/\,/gi, '').toLowerCase().split(' '));
+    };
+    return pointsArray;
+  };
+
+  str = str.toLowerCase().split('');
+  
+  for (let i = 0; i < str.length; i++) {
+    
+    const element = str[i];
+    
+    for (let j = 0; j < organizeInputs('A, e, i, o, u, l, n, r, s, t', 'd, g', 'b, c, m, p', 'f, h, v, w, y', 'k', 'j, x', 'q, z').length; j++) {
+      
+      const pointChecker = organizeInputs('a, e, i, o, u, l, n, r, s, t', 'd, g', 'b, c, m, p', 'f, h, v, w, y', 'k', 'j, x', 'q, z')[j];
+        for (let k = 0; k < pointChecker.length; k++) {
+          const letterChecker = pointChecker[k];
+          if(element === letterChecker && j === 0){
+            pointsAwarded = pointsAwarded + 1
+          } else if(element === letterChecker && j === 1){
+              pointsAwarded = pointsAwarded + 2
+          } else if(element === letterChecker && j === 2){
+              pointsAwarded = pointsAwarded + 3
+          } else if(element === letterChecker && j === 3){
+              pointsAwarded = pointsAwarded + 4 
+          } else if(element === letterChecker && j === 4){
+              pointsAwarded = pointsAwarded + 5
+          } else if(element === letterChecker && j === 5){
+              pointsAwarded = pointsAwarded + 8
+          } else if(element === letterChecker && j === 6){
+              pointsAwarded = pointsAwarded + 10
+          };
+        };
+    };
+  };
+  return pointsAwarded;
 };
